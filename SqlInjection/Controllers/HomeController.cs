d177aec2a09a89b031f1de91f4f291c30a21f5ef
Vuong <vuonquocnguyen2604@gmail.com>
2025-11-03 13:18:06 +0100

    added sqlintjtection

---
+++ SqlInjection/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SqlInjection.Database;
+using SqlInjection.Models;
+using System.Text.RegularExpressions;
+
+namespace SqlInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        private bool ValidateText(string text)
+        {
+            string pattern = "[^a-zA-Z]";
+            Regex reg = new Regex(pattern);
+
+            return reg.IsMatch(text);
+        }
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("SearchStudentUnsecure")]
+        public async Task<IActionResult> SearchStudentUnsecure(string name)
+        {
+            ValidateText(name);
+
+            var conn = _context.Database.GetDbConnection();
+            var query = "SELECT FirstName, LastName FROM Student WHERE FirstName Like '%" + name + "%'";
+            IEnumerable<Student> students;
+
+            try
+            {
+                await conn.OpenAsync();
+                students = await conn.QueryAsync<Student>(query);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+            return Ok(students);
+        }
+
+        [HttpGet("SearchStudentSecure")]
+        public async Task<IActionResult> SearchStudentSecure(string name)
+        {
+            var conn = _context.Database.GetDbConnection();
+            var query = "SELECT FirstName, LastName FROM Student WHERE FirstName Like @name";
+            IEnumerable<Student> students;
+
+            try
+            {
+                await conn.OpenAsync();
+                students = await conn.QueryAsync<Student>(query, new { name });
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+            return Ok(students);
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var students = from s in _context.Students
+                               select s;
+                return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), 1, 10));
+            }
+            catch
+            {
+                return View(new PaginatedList<Student>(new List<Student>(), 1, 1, 10));
+            }
+
+        }
+
+        [HttpGet("RecreateDatabase")]
+        public async Task<ActionResult> RecreateDatabase()
+        {
+            await _context.Database.EnsureDeletedAsync();
+
+            await _context.Database.MigrateAsync();
+            await _context.Database.EnsureCreatedAsync();
+            await DbInitializer.Initialize(_context);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
